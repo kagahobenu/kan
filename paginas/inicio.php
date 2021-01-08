7e9f843c710a9ddbb302ca1e11bbc49361ae0c88
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-7TUSQ40>
2021-01-08 09:52:20 -0300

    kawnkan

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -18,35 +18,7 @@
         </div>
         <div class="col">
             <h1>Site em Manutenção</h1>
-            <p class="text-justify">Lorem Ipsum is simply dummy text of 
-                the printing and typesetting industry. Lorem Ipsum has 
-                been the industry's standard dummy text ever since the 
-                1500s, when an unknown printer took a galley of type and 
-                scrambled it to make a type specimen book. remaining 
-                essentially unchanged. It was popularised in the 1960s 
-                with the release ocimen book. It has survived not only 
-                five centuries, but also the leap into electronic 
-                typesetting, remaining essentially unchanged. It was 
-                popularised in the 1960s with the release ocimen book. 
-                It has survived not only five centuries, but also the 
-                leap into ease ocimen book. It has survived not only 
-                five centuries, but also the leap into electronic 
-                typesetting, remaining essentially unchanged. It wasIt 
-                has survived not only five centuries, but also the leap 
-                into electronic typesetting, remaining essentially 
-                unchanged. It was popularised in the 1960s with the 
-                release ocimen book. It has survived not only five 
-                centuries, but also the leap into electronic 
-                typesetting, remaining essentially unchanged. It was 
-                popularised in the 1960s with the release ocimen book. 
-                It has survived not only five centuries, but also the 
-                leap into ease ocimen book. It has survived not only 
-                five centuries, but also the leap into electronic 
-                typesetting, remaining essentially unchanged. It was 
-                popularised in the 1960s with the release of Letraset 
-                sheets containing Lorem Ipsum passages, and more 
-                recently with desktop publishing software like Aldus 
-                PageMaker including versions of Lorem Ipsum.
+            <p class="text-justify">ççççççççççççççççççççççççç
             </p>
         </div>    
     </div>
